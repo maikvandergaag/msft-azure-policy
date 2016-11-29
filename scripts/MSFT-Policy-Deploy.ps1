f3475bee8300fcb75b67e7ac6e940a1aa51939b0
Maik van der Gaag <maik.vandergaag@motion10.com>
2016-11-29 21:14:55 +0100

    script change

--- scripts/MSFT-Policy-Deploy.ps1
+++ scripts/MSFT-Policy-Deploy.ps1
@@ -5,6 +5,8 @@ $resourceGroup = Read-Host "Specify the resource group";
 
 #Login to the Azure Resource Management Account
 Login-AzureRmAccount
+
+#Let the user choose the right subscrition
 Write-Host "---------------------------------------------------------------------"
 Write-Host "Your current subscriptions: " -ForegroundColor Yellow
 Get-AzureRMSubscription
@@ -13,8 +15,12 @@ $sub = Read-Host
 Set-AzureRmContext -SubscriptionId $sub
 clear
 
+$subId = (Get-AzureRmContext).Subscription.SubscriptionId
+$subName = (Get-AzureRmContext).Subscription.SubscriptionName
+
+Write-Host "Policy is applied to the resource group: $resourceGroup in subscription: $subName"
 $policy = New-AzureRmPolicyDefinition -Name $policyName -Description $policyDescription -Policy $policyFile;
 
 #Assign the Azure Policy
-New-AzureRmPolicyAssignment -Name regionPolicyAssignment -PolicyDefinition $policy -Scope "/subscriptions/$sub/resourceGroups/$resourcegroup"
+New-AzureRmPolicyAssignment -Name $policyName -PolicyDefinition $policy -Scope "/subscriptions/$sub/resourceGroups/$resourcegroup"
 
