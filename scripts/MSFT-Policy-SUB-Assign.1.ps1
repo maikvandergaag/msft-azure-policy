4f95c74c05648986dda29f6035f73de0c20101c1
Maik van der Gaag <maik.vandergaag@motion10.com>
2016-11-29 22:09:35 +0100

    name change + readme

---
+++ scripts/MSFT-Policy-SUB-Assign.1.ps1
@@ -0,0 +1,25 @@
+$policyName = Read-Host "Specify the name of the policy";
+$policyDescription = Read-Host "Specify the description of the policy"
+$policyFile = Read-Host "Path to json policy file";
+
+#Login to the Azure Resource Management Account
+Login-AzureRmAccount
+
+#Let the user choose the right subscrition
+Write-Host "---------------------------------------------------------------------"
+Write-Host "Your current subscriptions: " -ForegroundColor Yellow
+Get-AzureRMSubscription
+Write-Host "Enter the Subscription ID to deploy to: " -ForegroundColor Green
+$sub = Read-Host 
+Set-AzureRmContext -SubscriptionId $sub
+clear
+
+$subId = (Get-AzureRmContext).Subscription.SubscriptionId
+$subName = (Get-AzureRmContext).Subscription.SubscriptionName
+
+Write-Host "Policy is applied to the resource group: $resourceGroup in subscription: $subName"
+$policy = New-AzureRmPolicyDefinition -Name $policyName -Description $policyDescription -Policy $policyFile;
+
+#Assign the Azure Policy
+New-AzureRmPolicyAssignment -Name $policyName -PolicyDefinition $policy -Scope "/subscriptions/$sub"
+
